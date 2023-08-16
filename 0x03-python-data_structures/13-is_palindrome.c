13012fdb0aa6d790bf90eb606d79f2fce792669b
DAKUBZ <tinekmarty@gmail.com>
2023-08-16 03:27:56 +0000

    updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,76 +1,42 @@
-uth: Mbah Nkemdinma
- */
-
 #include "lists.h"
-
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
+#include <stdio.h>
 
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- *
- * Return: A pointer to the head of the reversed list.
- */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *node = *head, *next, *prev = NULL;
-
-	while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
- */
+* is_palindrome - Checks if a singly linked list is a palindrome.
+*
+* @head: A pointer to a singly linked list
+*
+* Return: 1 if palindrome, 0 if not
+*/
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
+listint_t *slow = *head, *ahead = *head;
+listint_t *prev = NULL, *temp;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+if (!head)
+return (1);
 
-	tmp = *head;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;
-	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
-			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
-	}
-	reverse_listint(&mid);
-
-	return (1);
+/* Locate the middle node using ahead and slow pointers*/
+while (ahead && ahead->next)
+{
+ahead = ahead->next->next;
+temp = slow;
+slow = slow->next;
+temp->next = prev;
+prev = temp;
 }
 
+/* Handle odd list*/
+if (ahead)
+slow = slow->next;
+
+/* Compare first and last half */
+while (slow && prev)
+{
+if (slow->n != prev->n)
+return (0);
+slow = slow->next;
+prev = prev->next;
+}
+return (1);
+}
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
-x_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "")
-        print()
+#!/usr/bin/python3
 
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i > 0:
+                print(" ", end="")
+            print("{:d}".format(row[i]), end="")
+        print()
