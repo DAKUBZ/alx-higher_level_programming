6cad39ca78fedd12e35b41a6b3ac1605f1af38e5
DAKUBZ <tinekmarty@gmail.com>
2023-10-19 15:53:19 -0700

    Intro

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates a table in current database
+-- Query to create a table called first_table in the current database in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
