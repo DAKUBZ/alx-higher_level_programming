794b0ad8d59a3571964dfc7a43dcf8c88d8fa4d7
DAKUBZ <tinekmarty@gmail.com>
2023-11-13 20:49:17 +0000

    java

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const lang of languages) console.log(lang);
+// script thatprints 3 lines
+let myVar = 'C is fun';
+console.log(myVar);
+myVar = 'Python is cool';
+console.log(myVar);
+myVar = 'JavaScript is amazing';
+console.log(myVar);
