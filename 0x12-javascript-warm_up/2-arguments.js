e9280eca9531afa6bca8e01496a2bd66097a6a4d
DAKUBZ <tinekmarty@gmail.com>
2023-11-13 21:17:00 +0000

    Java

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
-// handles arguments bassed on command line
-const argCount = process.argv.length - 2;
-if (argCount === 0) {
+const argc = process.argv.length;
+if (argc === 2) {
   console.log('No argument');
-} else if (argCount === 1) {
+} else if (argc === 3) {
   console.log('Argument found');
-} else {
+} else if (argc > 3) {
   console.log('Arguments found');
 }
